40611450850f8bfefd340deb5d42e467abf4dab1
mindblinds <>
2019-01-28 16:12:04 +0900

    Updates

---
+++ precache-manifest.0ddf97c5f7fa301f5ac566c37b36d587.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b983b9d4e14f6c90df8",
+    "url": "/static/css/main.e14d0d1d.chunk.css"
+  },
+  {
+    "revision": "6b983b9d4e14f6c90df8",
+    "url": "/static/js/main.6b983b9d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9dac0876717fb06ba73363f933363f6f",
+    "url": "/static/media/face.9dac0876.png"
+  },
+  {
+    "revision": "1179bc3609f10eb3b9059c000d20a4b0",
+    "url": "/static/media/backimage.1179bc36.jpg"
+  },
+  {
+    "revision": "b74f4017b9118732619039d482fea23e",
+    "url": "/index.html"
+  }
+];
